43be81738952713a72d6b6208f4c0f2348151694
Антон Кулик <anton.smile.1998@gmail.com>
2022-08-29 14:28:51 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Антон Кулик on 29.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
